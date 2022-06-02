ec9e4baf8641bf291f5d193e228dcb8e5a16bf74
Robert Di Ninno <robertdininno@gmail.com>
2022-06-02 14:44:04 +0000

    add secuity measures for malicious input

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -7,6 +7,12 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+//INPUT
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 pool.connect()
 .then(() => {
   console.log("DB connected");
@@ -15,13 +21,16 @@ pool.connect()
   console.log("Error connecting DB: ", error);
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+
+  pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,12 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+//INPUT
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 pool.connect()
 .then(() => {
   console.log("DB connected");
@@ -15,15 +21,16 @@ pool.connect()
   console.log("Error connecting DB: ", error);
 });
 
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
